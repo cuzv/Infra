1985a5c6aed690d1be4122fed47862e82084ff54
Shaw <cuzval@gmail.com>
2022-01-02 21:51:22 +0800

    Fix access control

--- Sources/PropertyWrappers/Observable.swift
+++ Sources/PropertyWrappers/Observable.swift
@@ -3,7 +3,7 @@ import Foundation
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, macCatalyst 13.0, *)
 @propertyWrapper
 public final class Observable<Value>: ObservableObject {
-  @Published public var innerValue: Value
+  @Published private var innerValue: Value
 
   public init(wrappedValue: Value) {
     _innerValue = Published(wrappedValue: wrappedValue)
