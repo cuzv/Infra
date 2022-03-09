a33d0fc6441a9e8f877b51b9cbe2b8d55fb4d886
Shaw <cuzval@gmail.com>
2022-03-09 23:40:16 +0800

    Add dropNils

--- Sources/Collections/Sequence+.swift
+++ Sources/Collections/Sequence+.swift
@@ -256,6 +256,12 @@ extension Sequence {
   public func compactMap<E>(_ type: E.Type) -> [E] {
     compactMap { $0 as? E }
   }
+
+  @inline(__always)
+  public func dropNils<Wrapped>()
+  -> [Wrapped] where Element == Wrapped? {
+    compactMap { $0 }
+  }
 }
 
 extension LazySequence {
