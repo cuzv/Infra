45dc4a8131390db576bd339e66fa8571d91affa4
Shaw <cuzval@gmail.com>
2022-12-01 21:56:10 +0800

    Amend

--- Sources/Swift/Silent.swift
+++ Sources/Swift/Silent.swift
@@ -14,3 +14,21 @@ public func silent(perform: () async throws -> Void) async {
     debugPrint(error)
   }
 }
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, macCatalyst 13.0, *)
+public extension Task {
+  @discardableResult
+  static func silent(
+    priority: TaskPriority? = nil,
+    operation: @escaping () async throws -> Void
+  ) -> Self
+  where Success == Void, Failure == Never {
+    .init(priority: priority) {
+      do {
+        try await operation()
+      } catch {
+        debugPrint(error)
+      }
+    }
+  }
+}
