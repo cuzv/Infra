03856a2afe0168fc2b18c8ddc55a723235f8743b
Shaw <cuzval@gmail.com>
2024-01-24 14:26:07 +0800

    Format

--- Sources/Foundation/NSError+.swift
+++ Sources/Foundation/NSError+.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public extension NSError {
   static let unknown = NSError.from(desc: "Unknown error")
-  
+
   static func from(domain: String = "com.redrainlab.default", code: Int = -1, desc: String) -> NSError {
     NSError(domain: domain, code: code, userInfo: [NSLocalizedDescriptionKey: desc])
   }
