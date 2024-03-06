6cdd0f781bdb084daf68aa2ce6b53aafa1dc9cdd
Shaw <>
2024-03-06 09:41:32 +0800

    Upgrade sink

--- Sources/Combine/Subscriber/Subscribers.Sink.swift
+++ Sources/Combine/Subscriber/Subscribers.Sink.swift
@@ -3,9 +3,11 @@ import Combine
 
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, macCatalyst 13.0, *)
 public extension Publisher {
-  func sink() -> AnyCancellable {
-    sink { _ in
-    } receiveValue: { _ in
+  func sink(_ prefix: String? = nil) -> AnyCancellable {
+    if let prefix {
+      print(prefix).sink(receiveCompletion: { _ in }, receiveValue: { _ in })
+    } else {
+      sink(receiveCompletion: { _ in }, receiveValue: { _ in })
     }
   }
 }
